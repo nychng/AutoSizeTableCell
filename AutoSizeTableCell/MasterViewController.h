739f797ee376a2b86a4d3d4a96c635b4dc980684
Erlend Fagerheim <erlendf80@gmail.com>
2014-03-16 15:54:47 +0100

    Initial Commit

---
+++ AutoSizeTableCell/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  AutoSizeTableCell
+//
+//  Created by adnim on 16/03/14.
+//  Copyright (c) 2014 AppTakk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
---
+++ AutoSizeTableCell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutoSizeTableCell
+//
+//  Created by adnim on 16/03/14.
+//  Copyright (c) 2014 AppTakk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
